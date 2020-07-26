e80f99299d792f1b99a1be95029b31b40f934e39
vkazak <vikt.kazak@gmail.com>
2020-07-27 00:45:52 +0300

    Make bottom tab bar pretty

--- navigation/tab_navigation.js
+++ navigation/tab_navigation.js
@@ -2,20 +2,54 @@ import React from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DummiesNavigator } from "./dummies_navigation";
 import { PartiesNavigator } from "./parties_navigation";
+import { Icon } from 'react-native-elements';
+import { APP_COLOR } from '../styles';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = (props) => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator 
+            tabBarOptions={ tabBarOptions }
+            screenOptions={ screenOptions }
+            initialRouteName='Parties'
+        >
             <Tab.Screen
-                name='Party navigation'
-                component={PartiesNavigator}
-            />
-            <Tab.Screen
-                name='Dummies navigation'
+                name='Dummies'
                 component={DummiesNavigator}
             /> 
+            <Tab.Screen
+                name='Parties'
+                component={PartiesNavigator}
+            />
         </Tab.Navigator>
     )
 }
+
+const tabBarOptions = {
+    activeTintColor: APP_COLOR, 
+    inactiveTintColor: '#bbb', 
+    showLabel: false,
+    showIcon: true,
+};
+
+const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused, color }) => {
+        let iconName;
+        if (route.name === 'Parties') {
+            iconName = 'home';
+        }
+        else if (route.name === 'Dummies') {
+            iconName = 'user';
+        }
+        const iconSize = focused ? 27 : 22;
+        return (
+            <Icon 
+                name={ iconName }
+                size={ iconSize } 
+                color={ color }
+                type='font-awesome-5'
+            />
+        )
+    },
+  });
--- utils.js
+++ utils.js
@@ -2,13 +2,3 @@ const config = require('./config.json');
 
 export const makeFullUrl = route => config.serverUri + route;
 export const avatarUrl = makeFullUrl(config.avatarUrl);
-export const dummyToUser = (dummy) => {
-    return ({
-        _id: dummy._id,
-        name: dummy.name,
-        email: 'dummy@dummy.com',
-        photoUrl: avatarUrl,
-        firstName: '',
-        lastName: ''
-    })
-}
